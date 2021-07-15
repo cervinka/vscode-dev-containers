15df5d8f663664f767d4166aefd0cc28c7638121
Petr Cervinka <petr@petrcervinka.cz>
2021-07-15 19:31:12 +0200

    Update node-debian.sh

--- script-library/node-debian.sh
+++ script-library/node-debian.sh
@@ -22,7 +22,7 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
-# Ensure that login shells get the correct path if the user updated the PATH using ENV.
+echo Ensure that login shells get the correct path if the user updated the PATH using ENV.
 rm -f /etc/profile.d/00-restore-env.sh
 echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" > /etc/profile.d/00-restore-env.sh
 chmod +x /etc/profile.d/00-restore-env.sh
